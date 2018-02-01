e070f1cd87c20a4e4b66ed5223d567a0664e5064
Siddharth Kannan <kannan.siddharth12@gmail.com>
2018-02-01 23:51:45 +0530

    change from 4-space indentation to tab indentation

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
-    "github.com/joho/godotenv"
-    "os"
+	"os"
 )
 
 var GlobalDBSession = DialDB()
 
 func main() {
 
-    err := godotenv.Load()
-    if err != nil {
-        log.Print("Error loading .env file")
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Print("Error loading .env file")
+	}
 
 	router := NewRouter()
-    router.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	router.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 
-    log.Printf("Server started on %s", os.Getenv("PORT"))
-	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), router))
+	log.Printf("Server started on %s", os.Getenv("PORT"))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 
-    defer GlobalDBSession.Close()
+	defer GlobalDBSession.Close()
 }
