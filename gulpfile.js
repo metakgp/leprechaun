73b704c3740dd88e3477c518c46ec84d4d114877
Siddharth Kannan <kannan.siddharth12@gmail.com>
2018-02-02 00:08:08 +0530

    include the git commit that was built in the footer section

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,43 @@
 var gulp          = require("gulp");
 var fs            = require("fs");
 var include       = require("gulp-include");
+var replace       = require('gulp-replace');
 
 gulp.task("html", function() {
-    console.log("-- gulp is running task 'scripts'");
+	console.log("-- gulp is running task 'scripts'");
 
-    try {
-        fs.mkdirSync("dist");
-    } catch(e) {
-        // pass
-    }
+	var commit = "";
 
-    gulp.src("html/*.html")
-    .pipe(include())
-    .on('error', console.log)
-    .pipe(gulp.dest("dist/html"));
+	const exec = require('child_process').exec;
+	const child = exec('git rev-list HEAD | head -n1',
+	(error, stdout, stderr) => {
+		if (!error) {
+			commit = stdout;
+		}
 
-    gulp.src("templates/*.html")
-    .pipe(include())
-    .on('error', console.log)
-    .pipe(gulp.dest("dist/templates"));
+		var head_str = "";
+		if (commit.length > 0) {
+			head_str = " (" + commit.substr(0, 6) + ")";
+		}
+
+		try {
+			fs.mkdirSync("dist");
+		} catch(e) {
+			// pass
+		}
+
+		gulp.src("html/*.html")
+		.pipe(include())
+		.pipe(replace("{{git_head}}", head_str))
+		.on('error', console.log)
+		.pipe(gulp.dest("dist/html"));
+
+		gulp.src("templates/*.html")
+		.pipe(include())
+		.on('error', console.log)
+		.pipe(gulp.dest("dist/templates"));
+
+	});
 });
 
 gulp.task("default", ["html"]);
