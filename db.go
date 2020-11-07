b11bbfebc32cac4eee4f48647ea624ccd9201256
Siddharth Kannan <mail@siddharthkannan.in>
2020-11-07 23:55:42 +0900

    Use separate env variable when MongoDB_URI is empty
    Required for migration from mLab to Atlas:
    
    https://github.com/metakgp/leprechaun/issues/14

--- db.go
+++ db.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"gopkg.in/mgo.v2"
 	"os"
+
+	"gopkg.in/mgo.v2"
 )
 
 // DialDB() returns a mgo Session connected to the Database pointed to by the
 // DB_URL environment variable
 func DialDB() *mgo.Session {
-	session, err := mgo.Dial(os.Getenv("MONGODB_URI"))
+	mongoURI := os.Getenv("MONGODB_URI")
+	if mongoURI == "" {
+		mongoURI := os.Getenv("ATLAS_MONGODB_URI")
+	}
+	session, err := mgo.Dial(mongoURI)
 	if err != nil {
 		panic(err)
 	}
